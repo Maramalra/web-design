658ae20b9740a096751eebb241c6b6edfd633472
MARAM ALRADDADI <142776708+Maramalra@users.noreply.github.com>
2026-09-14 23:03:12 +0300

    project I worked on in the web design course

---
+++ style.js
@@ -0,0 +1,35 @@
+function addNumbers(){
+
+var x=prompt("ENTER 1st NUMBER ");
+var y=prompt("ENTER 2nd NUMBER ");
+
+document.getElementById("result").innerHTML=parseInt(x)+parseInt(y);
+
+}
+
+Function subtractNumbers(){
+
+Var x=prompt("ENTER 1st NUMBER ");
+Var y=prompt("ENTER 2nd NUMBER ");
+
+document.getElementById("result").innerHTML=parseInt(x)-parseInt(y);
+
+}
+
+Function multiplyNumbers(){
+
+Var x=prompt("ENTER 1st NUMBER ");
+Var y=prompt("ENTER 2nd NUMBER ");
+
+document.getElementById("result").innerHTML=parseInt(x)*parseInt(y);
+
+}
+
+Function divideNumbers(){
+
+Var x=prompt("ENTER 1st NUMBER ");
+Var y=prompt("ENTER 2nd NUMBER ");
+
+document.getElementById("result").innerHTML=parseInt(x)/parseInt(y);
+
+}
